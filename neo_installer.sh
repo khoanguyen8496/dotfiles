92246dd9ef3a3ff5291442aa49be5c42055903eb
khoanguyen8496 <khoanguyen8496@gmail.com>
2018-05-18 21:32:52 +0700

    update neo installer

--- neo_installer.sh
+++ neo_installer.sh
@@ -1,3 +1,13 @@
 #! /bin/sh
 cp -r nvim ~/.config
-
+python -m pip install --user neovim
+python3 -m pip install --user neovim
+mkdir build
+git clone --recursive https://github.com/cquery-project/cquery
+cd cquery && mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_EXPORT_COMPILE_COMMANDS=YES || exit 1
+make -j32
+sudo make install
+cd ../../
+sudo pip install python-language-server
+sudo npm install javascript-typescript-langserver
