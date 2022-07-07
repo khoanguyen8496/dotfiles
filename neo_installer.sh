c4fef3cc471965dcf90ab40b5e22c64e4f167694
Khoa Nguyen <nakhoa@amazon.com>
2022-07-07 21:46:52 +0800

    add script for headless sync nvim

--- neo_installer.sh
+++ neo_installer.sh
@@ -4,3 +4,4 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 python -m pip install --user neovim
 python3 -m pip install --user neovim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
